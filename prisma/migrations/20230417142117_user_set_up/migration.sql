3e4c7a1f130c2886ea9a79185243e9c7180c90bd
Chiamaka Ojiyi <chiamakaojiyi@gmail.com>
2023-04-17 15:33:33 +0100

    initial commit

---
+++ prisma/migrations/20230417142117_user_set_up/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "twoFA" BOOLEAN NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
