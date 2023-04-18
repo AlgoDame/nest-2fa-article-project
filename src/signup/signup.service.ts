cfa1b80b750e8a315832328cb3bdcf85b5f73ca9
Chiamaka Ojiyi <chiamakaojiyi@gmail.com>
2023-04-18 23:46:43 +0100

    signup flow done

--- src/signup/signup.service.ts
+++ src/signup/signup.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './create-user.dto';
+import { hashPassword } from '../utils/passwordHasher';
+import _ from 'underscore';
 
 @Injectable()
 export class SignupService {
   constructor(private prisma: PrismaService) {}
 
-  signUp(createUserDto: CreateUserDto) {
-    return this.prisma.user.create({ data: createUserDto });
+  async signUp(createUserDto: CreateUserDto) {
+    createUserDto.password = await hashPassword(createUserDto.password);
+    const user = await this.prisma.user.create({ data: createUserDto });
+    return _.omit(user, 'password');
   }
 }
